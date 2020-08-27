7b3a42a3dee50f4855a3a3d6df1ed5b18a844cd1
An Tran <tran.binhan@gmail.com>
2020-08-27 18:19:18 +0800

    Add 07

--- SwiftVietnam/SwiftVietnam/ContentView.swift
+++ SwiftVietnam/SwiftVietnam/ContentView.swift
@@ -17,71 +17,91 @@ extension URL: Identifiable {
 }
 
 struct ContentView: View {
-    static let feedURL = URL(string: "https://swiftvietnam.com/feed.rss")!
 
-    @State var newsItems: [NewsItem] = []
-    @State var link: URL? = nil
+    class ViewModel: ObservableObject {
+        static let feedURL = URL(string: "https://swiftvietnam.com/feed.rss")!
+
+        @Published private(set) var newsItems: [NewsItem] = []
+        @Published private(set) var link: URL? = nil
+
+        func select(_ link: URL?) {
+            self.link = link
+        }
+
+        func loadFeed() {
+            let parser = FeedParser(URL: Self.feedURL)
+
+            // Parse asynchronously, not to block the UI.
+            parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { result in
+                switch result {
+                case .success(let feed):
+                    guard let rssFeed = feed.rssFeed else {
+                        print("Feed ist empty")
+                        return
+                    }
+                    self.parseFeed(rssFeed)
+                case .failure(let error):
+                    print("Error: \(error)")
+                }
+            }
+        }
+
+        private func parseFeed(_ feed: RSSFeed) {
+            let newsItems = feed.items?.compactMap { rssFeedItem -> NewsItem? in
+                guard let title = rssFeedItem.title,
+                      let link = rssFeedItem.link,
+                      let url = URL(string: link) else {
+                    return nil
+                }
+
+                return NewsItem(
+                    title: title,
+                    link: url
+                )
+            }
+
+            // Go back the the main thread to update the UI.
+            DispatchQueue.main.async {
+                self.newsItems = newsItems ?? []
+            }
+        }
+    }
+
+    @ObservedObject private var viewModel = ViewModel()
+
+    init() {
+        viewModel.loadFeed()
+    }
 
     var body: some View {
         NavigationView {
             List {
-                ForEach(newsItems, id: \.self) { item in
+                ForEach(viewModel.newsItems, id: \.self) { item in
                     Button(action: {
-                        self.link = item.link
+                        viewModel.select(item.link)
                     }) {
                         NewsItemView(item: item)
                     }
                 }
             }
-            .sheet(item: $link, content: { link in
+            .sheet(item: .init(
+                get: {
+                    viewModel.link
+                },
+                set: {
+                    viewModel.select($0)
+                }
+            ), content: { link in
                 SafariView(url: link)
             })
             .navigationTitle("Swift Việt Nam")
             .navigationBarItems(
                 trailing: Button(action: {
-                    self.loadFeed()
+                    viewModel.loadFeed()
                 }) {
                     Text("Load")
                 }
             )
-        }
-    }
-
-    private func loadFeed() {
-        let parser = FeedParser(URL: Self.feedURL)
-
-        // Parse asynchronously, not to block the UI.
-        parser.parseAsync(queue: DispatchQueue.global(qos: .userInitiated)) { result in
-            switch result {
-            case .success(let feed):
-                guard let rssFeed = feed.rssFeed else {
-                    print("Feed ist empty")
-                    return
-                }
-                self.parseFeed(rssFeed)
-            case .failure(let error):
-                print("Error: \(error)")
-            }
-        }
-    }
-
-    private func parseFeed(_ feed: RSSFeed) {
-        let newsItems = feed.items?.compactMap { rssFeedItem -> NewsItem? in
-            guard let title = rssFeedItem.title,
-                  let link = rssFeedItem.link,
-                  let url = URL(string: link) else {
-                return nil
-            }
-
-            return NewsItem(
-                title: title,
-                link: url
-            )
-        }
-
-        // Go back the the main thread to update the UI.
-        DispatchQueue.main.async {
-            self.newsItems = newsItems ?? []
         }
     }
 }
